564513fa401a32a50788f2e3049b6b43fb1a17ee
DIPAK GHUGE <deepghuge@gmail.com>
2020-07-04 16:21:09 +0530

    Intial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  
+})
+const port = process.env.port || 5000;
+app.listen(port,()=>{
+  console.log(`App Running on port ${port}`);
+})

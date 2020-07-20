2f281448b4406f8b08dd1a822d983d8c06520008
DIPAK GHUGE <deepghuge@gmail.com>
2020-07-21 03:00:44 +0530

    Done with basics

--- config/appConfig_Prod.js
+++ config/appConfig_Prod.js
@@ -5,10 +5,3 @@ module.exports ={
   googleClientID:process.env.GOOGLE_CLIENT_ID,
   googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
 }
-// module.exports ={
-//   username:'deepak',
-//   password:'deepak',
-//   dbName: 'book',
-//   googleClientID:"791139257043-gv2js6btqsc1kcfcgcovsk5m7kq837ji.apps.googleusercontent.com",
-//   googleClientSecret:"2Q-ieUtIYkGguhFvnb_BZCrv"
-// }
